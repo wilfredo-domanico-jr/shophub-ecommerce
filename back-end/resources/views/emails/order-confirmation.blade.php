d6182690e86147a2e2edbe71f43545536d1d4723
Wilfredo Domanico <wilfredo.domanico.jr@gmail.com>
2026-07-08 21:47:05 +0800

    feat: brand the order confirmation and status-update emails
    Publish the default markdown mail theme and restyle it with ShopHub's
    orange accent, add an order-summary panel and clearer totals to the
    confirmation email, and add a color-coded status badge with a
    contextual message to the status-update email.

--- back-end/resources/views/emails/order-confirmation.blade.php
+++ back-end/resources/views/emails/order-confirmation.blade.php
@@ -1,7 +1,13 @@
 <x-mail::message>
 # Thanks for your order, {{ $order->customer_name }}!
 
-Your order **{{ $order->order_number }}** has been received and is being prepared.
+Your order has been received and is being prepared. Here's a summary for your records.
+
+<x-mail::panel>
+**Order Number:** {{ $order->order_number }}<br>
+**Order Date:** {{ $order->created_at->format('F j, Y') }}<br>
+**Payment Method:** {{ $order->payment_method }}
+</x-mail::panel>
 
 <x-mail::table>
 | Item | Qty | Price | Subtotal |
@@ -9,17 +15,16 @@ Your order **{{ $order->order_number }}** has been received and is being prepare
 @foreach ($order->items as $item)
 | {{ $item->product_name }} | {{ $item->quantity }} | ₱{{ number_format($item->product_price, 2) }} | ₱{{ number_format($item->subtotal, 2) }} |
 @endforeach
+| | | **Subtotal** | ₱{{ number_format($order->subtotal, 2) }} |
+| | | **Shipping** | ₱{{ number_format($order->shipping_fee, 2) }} |
+| | | **Total** | **₱{{ number_format($order->total, 2) }}** |
 </x-mail::table>
 
-**Subtotal:** ₱{{ number_format($order->subtotal, 2) }}
-**Shipping:** ₱{{ number_format($order->shipping_fee, 2) }}
-**Total:** ₱{{ number_format($order->total, 2) }}
-
-**Payment Method:** {{ $order->payment_method }}
-**Shipping Address:** {{ $order->shipping_address }}
+**Shipping Address**<br>
+{{ $order->shipping_address }}
 
 You can track your order anytime using your order number and email address.
 
-Thanks,<br>
+Thanks for shopping with us,<br>
 {{ config('app.name') }}
 </x-mail::message>
--- back-end/resources/views/emails/order-status-updated.blade.php
+++ back-end/resources/views/emails/order-status-updated.blade.php
@@ -1,9 +1,26 @@
+@php
+$statusColor = match ($order->status) {
+    'delivered' => '#16a34a',
+    'cancelled' => '#dc2626',
+    default => '#ff6b35',
+};
+
+$statusMessage = match ($order->status) {
+    'processing' => 'We\'re getting your order ready.',
+    'shipped' => 'Your order is on its way!',
+    'delivered' => 'Your order has been delivered. Enjoy!',
+    'cancelled' => 'This order has been cancelled.',
+    default => 'We\'ll notify you again as soon as it progresses.',
+};
+@endphp
 <x-mail::message>
 # Order {{ $order->order_number }} update
 
 Hi {{ $order->customer_name }}, your order status has changed to:
 
-## {{ ucfirst($order->status) }}
+## <span style="color: {{ $statusColor }};">{{ ucfirst($order->status) }}</span>
+
+{{ $statusMessage }}
 
 <x-mail::table>
 | Item | Qty | Subtotal |
@@ -11,9 +28,8 @@ Hi {{ $order->customer_name }}, your order status has changed to:
 @foreach ($order->items as $item)
 | {{ $item->product_name }} | {{ $item->quantity }} | ₱{{ number_format($item->subtotal, 2) }} |
 @endforeach
+| | **Total** | **₱{{ number_format($order->total, 2) }}** |
 </x-mail::table>
-
-**Total:** ₱{{ number_format($order->total, 2) }}
 
 Thanks,<br>
 {{ config('app.name') }}
